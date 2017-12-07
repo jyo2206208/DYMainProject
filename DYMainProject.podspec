c804b9fa8ec84e23df0e67cf34efea24a98f2554
shaggon <shaggon.du@farfetch.com>
2017-12-07 23:12:27 +0800

    0.0.2

--- DYMainProject.podspec
+++ DYMainProject.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'DYMainProject'
-  s.version          = '0.0.1'
+  s.version          = '0.0.2'
   s.summary          = 'A meaningful short description of DYMainProject.'
 
 # This description is used to generate tags and improve search results.
--- DYMainProject_Example/FCNAppDelegate.m
+++ DYMainProject_Example/FCNAppDelegate.m
@@ -22,14 +22,6 @@
     return YES;
 }
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options{
-//    [[FFCallCenter sharedInstance] performActionWithUrl:url completion:^(NSDictionary *info) {
-//        NSLog(@"Do something after openURL");
-//    }];
-    
-    return YES;
-}
-
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
